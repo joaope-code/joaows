5074e467a0d9276286765d0e807764e56f85982f
joaope-code <jaumundergroud@gmail.com>
2021-07-06 11:24:31 -0300

    block s

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int value1 = 10;
+        int value2 = 4;
+        if (value1 > value2)
+         System.out.println("value 1 is bigger");
+        else
+          System.out.println("value 1 in not bigger");
+        if (value1 == value2)
+            System.out.println("valve 1 and valve 2 are equal");
+    }
+}
